f78c0f37a95e36cfafd7d805430c91d20985cb2b
marcenillas <marcenillas@gmail.com>
2025-02-21 11:15:21 -0300

    Initial commit

---
+++ helloWorldApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'helloWorldApp',
+  webDir: 'www'
+};
+
+export default config;
